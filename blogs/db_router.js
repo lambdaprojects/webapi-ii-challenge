43977141f0781c0e4c1d981ca06b07be9a5d7b53
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-28 17:22:15 -0500

    FintPostComments completed

--- blogs/db_router.js
+++ blogs/db_router.js
@@ -57,4 +57,38 @@ router.put("/:id", async (req, res) => {
     res.status(500).json({ ErrorMessage: "Error in updation" });
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ SuccessMessage: "The blog is deleted" });
+    } else {
+      res.status(404).json({ message: "The blog cannot be deleted" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error removing the blog"
+    });
+  }
+});
+
+router.get("/:id/posts", async (req, res) => {
+  try {
+    const posts = await db.findPostComments(req.params.id);
+    if (posts.length > 0) {
+      res.status(200).json(posts);
+    } else {
+      res.status(404).json({
+        ErrorMessage: `There are no posts associated with the id ${id}`
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error in retrieving post comments"
+    });
+  }
+});
 module.exports = router;
