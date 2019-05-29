3fca84ade032c0066be2b15207161c07ccb518b9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-28 19:43:43 -0500

    Stretch: Added cors and creating a react app

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const databaseRouter = require("./posts/db_router.js");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("    <h2>Lambda Chat API</h2><p>Welcome to the chat API</p>");
